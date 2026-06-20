0f236d9c02b441caa013fef43dd93499804ff328
abbqsjgl06-cloud <abbqsjgl06@gmail.com>
2026-06-20 14:28:26 +0700

    Add files via upload

---
+++ detail_history.js
@@ -0,0 +1,182 @@
+let data =
+JSON.parse(
+localStorage.getItem(
+"selectedHistory"
+)
+);
+
+document.getElementById(
+"judulHistory"
+).innerHTML =
+
+data.kategori +
+" - " +
+data.type +
+" - " +
+data.tanggal;
+
+
+let html = "";
+
+data.items.forEach((item,index)=>{
+
+html += `
+
+<tr>
+
+<td>${item.nomor}</td>
+
+<td>${item.kode}</td>
+
+<td>${item.item}</td>
+
+<td>${item.konv}</td>
+
+<td>${item.uom}</td>
+
+<td>
+
+<input
+type="number"
+class="qty-input"
+id="qty_${index}"
+value="${item.pcs_gr}">
+
+</td>
+
+</tr>
+
+`;
+
+});
+
+document.getElementById(
+"tableBody"
+).innerHTML = html;
+
+
+
+function updateData(){
+
+data.items.forEach((item,index)=>{
+
+item.pcs_gr = Number(
+
+document.getElementById(
+"qty_"+index
+).value
+
+);
+
+});
+
+let historyData =
+
+JSON.parse(
+localStorage.getItem(
+"historyStock"
+)
+) || [];
+
+let index = historyData.findIndex(
+
+x =>
+
+x.tanggal === data.tanggal &&
+
+x.kategori === data.kategori &&
+
+x.type === data.type
+
+);
+
+if(index !== -1){
+
+historyData[index] = data;
+
+localStorage.setItem(
+
+"historyStock",
+
+JSON.stringify(historyData)
+
+);
+
+}
+
+localStorage.setItem(
+
+"selectedHistory",
+
+JSON.stringify(data)
+
+);
+
+alert(
+"Data berhasil diperbarui"
+);
+
+}
+
+
+
+function exportHistoryExcel(){
+
+let excelData = [];
+
+data.items.forEach(item=>{
+
+excelData.push({
+
+"No":item.nomor,
+
+"Kode":item.kode,
+
+"Item":item.item,
+
+"Konv":item.konv,
+
+"UOM":item.uom,
+
+"PCS/Gr":item.pcs_gr
+
+});
+
+});
+
+let wb =
+XLSX.utils.book_new();
+
+let ws =
+XLSX.utils.json_to_sheet(
+excelData
+);
+
+XLSX.utils.book_append_sheet(
+wb,
+ws,
+"Stock Opname"
+);
+
+let namaFile =
+
+"SO_" +
+
+data.kategori +
+
+"_" +
+
+data.type +
+
+"_" +
+
+data.tanggal +
+
+".xlsx";
+
+XLSX.writeFile(
+wb,
+namaFile
+);
+
+}
